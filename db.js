39ab477bf746532407abf6d498cb7bbe7ee56c4d
Enzo <cayetano.enzo@icloud.com>
2025-04-25 20:41:32 -0700

    fixed mongoose

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ async function connectDB()
 {
   try
   {
-    await mongoose.connect(process.env.MONGODB_URL, {
+    await mongoose.connect(process.env.MONGO_URI, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 		});
